846339d7049de6e52bd2ae5e5e710ade823a2de9
Adriano J <adrianojbarros5@gmail.com>
2024-03-07 14:28:29 -0300

    teste com variaveis

--- java/JavaApplication2/src/script/estudos2.java
+++ java/JavaApplication2/src/script/estudos2.java
@@ -1,5 +1,21 @@
-public class estudos2 {
+// O script a seguir é para testes de declaração de variavel
 
+public class estudos2 {
+     
     
+    public static void main(String[]args){
+    int idade, telefone;
+    int altura;
     
+    idade = 18;
+    altura = 1;
+    telefone = 99999999;
+           
+    System.out.print("--== 𝘛𝘌𝘚𝘛𝘌 𝘋𝘌 𝘝𝘈𝘙𝘐𝘈𝘝𝘌𝘓 ==--");
+    System.out.print("\n");
+    System.out.print("A idade é: " + idade + "\n");
+    System.out.print("A altura é: " + altura + "\n");
+    System.out.print("O telefone é: " + telefone + "\n");
+    
+    }
 }
