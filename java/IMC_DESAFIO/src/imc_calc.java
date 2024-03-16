3b8f097cd85dac9aff6ef71cb68cfe63e008ba63
Adriano J <adrianojbarros5@gmail.com>
2024-03-16 19:22:33 -0300

    delete java projeto

--- java/IMC_DESAFIO/src/imc_calc.java
+++ java/IMC_DESAFIO/src/imc_calc.java
@@ -1,93 +1,34 @@
-/*
-Construa um algoritmo que calcule o peso ideal de uma pessoa. Dados de entrada: 
-VARIAVEIS NECESSARIA: altura e sexo. 
-
-Fórmulas para cálculo do peso: peso ideal de 
-
-homem = (72,7 x altura) - 58
-
-mulher = (62,1 x altura) - 44,7
-
-*/
-
-
 import java.util.Scanner;
-public class imc_calc {
-    public static void main (String []args){
-    Scanner sc = new Scanner(System.in);
-    
-    
-    System.out.println("|> Digite seu sexo (F ou H): ");
-    String sexo_usuario = sc.nextLine();
-    
-    if(sexo_usuario.equals("f")){
-        System.out.println("|> Digite seu nome: ");
-        String nome_usuario = sc.nextLine();
-        System.out.println("|> Por favor, digite seu peso: ");
-        int peso_usuario = sc.nextInt();
-        System.out.println("|> Por favor, digite sua altura (em CM): ");
-        int altura_usuario = sc.nextInt();
-        
-        // Parte em que o usuario irá ver as informações coletadas
-        System.out.println("\n --==Verificando dados==-- ");
-        System.out.println("|> Nome: " + nome_usuario + "<|");
-        System.out.println("|> Sexo: " + sexo_usuario + "   <|");
-        System.out.println("|> Peso: " + peso_usuario + "   <|");
-        System.out.println("|> Altura: " + altura_usuario + " <|");
-        // FIM parte em que o usuario irá ver as informações coletadas
-        
-        System.out.println("Seu peso adequado é: ");
-        double peso_adequado_usuario;
-        peso_adequado_usuario = (62.1 * (altura_usuario / 100)) - 44.7;
-        System.out.print("======= " + peso_adequado_usuario + " =======" + "\n");
-            if(peso_adequado_usuario < 18.5){
-                System.out.println(nome_usuario + " você está abaixo do peso");
-            } 
-            if(peso_adequado_usuario >= 18.5 && peso_adequado_usuario < 24.9){
-                System.out.println(nome_usuario + " você está com peso normal");
-            }
-            if(peso_adequado_usuario >= 25 && peso_adequado_usuario < 29.9){
-                System.out.println(nome_usuario + " você está com sobrepeso");
-            } 
-                else {
-                    System.out.println("O paciente está obeso");
-                }
+
+public class imc_calc { // IGNORA ESSA CLASS, É APENAS PARA O PROJETO FUNCIONAR AQUI...
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        String NOME = sc.nextLine();
+        System.out.println("Obrigado, " + NOME);
+
+        System.out.println("[1] Masculino");
+        System.out.println("[2] Feminino");
+        System.out.print("Selecione o sexo da pessoa de acordo com as alternativas acima: ");
+        int num = sc.nextInt();
+
+        if (num == 1) {
+            System.out.println("Sexo Masculino");
+        } else if (num == 2) {
+            System.out.println("Sexo feminino");
         }
-    else if(sexo_usuario.equals("h")){
-        System.out.println("|> Digite seu nome: ");
-        String nome_user = sc.nextLine();
-        System.out.println("|> Por favor, digite seu peso: ");
-        int peso_usuario = sc.nextInt();
-        System.out.println("|> Por favor, digite sua altura (em CM): ");
-        int altura_usuario = sc.nextInt();
-        
-        // Parte em que o usuario irá ver as informações coletadas
-        System.out.println("\n --==Verificando dados==-- ");
-        System.out.println("|> Nome: " + nome_user + "      <|");
-        System.out.println("|> Sexo: " + sexo_usuario + "   <|");
-        System.out.println("|> Peso: " + peso_usuario + " <|");
-        System.out.println("|> Altura: " + altura_usuario + " <|");
-        // FIM parte em que o usuario irá ver as informações coletadas
-        
-        System.out.println("Seu peso adequado é ");
-        double peso_adequado_usuario;
-        peso_adequado_usuario = (72.7 * (altura_usuario / 100)) - 44.7;
-        System.out.print("======= " + peso_adequado_usuario + " =======" + "\n");
-        if(peso_adequado_usuario < 18.5){
-                System.out.println(nome_user + " você está abaixo do peso");
-            } 
-            if(peso_adequado_usuario >= 18.5 && peso_adequado_usuario < 24.9){
-                System.out.println(nome_user + " Você está com peso normal");
-            }
-            if(peso_adequado_usuario >= 25 && peso_adequado_usuario < 29.9){
-                System.out.println(nome_user + " Você está com sobrepeso");
-            } 
-            else {
-                System.out.println(nome_user + " Você está obeso\n");
-            }
-    }
-    else{
-        System.out.println("O valor digitado não é VALIDO...");
+
+        System.out.print("Agora digite sua altura (utilize o ponto para decimais, exemplo 1.70): ");
+        double altura = sc.nextDouble();
+
+        double pesoIdeal;
+
+        if (num == 1) {
+            pesoIdeal = (72.7 * altura) - 58;
+            System.out.println("O peso ideal para esse Homem é: " + pesoIdeal);
+        } else if (num == 2) {
+            pesoIdeal = (62.1 * altura) - 44.7;
+            System.out.println("O peso ideal para essa Mulher é: " + pesoIdeal);
         }
     }
 }
