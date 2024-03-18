21bd476631d93a4c6f3b7ffca5fb596052bd8ac3
Adriano J <adrianojbarros5@gmail.com>
2024-03-18 02:44:07 -0300

    Atv3 (FEITA)

--- java/atividade1/atividade1/src/Atv2.java
+++ java/atividade1/atividade1/src/Atv2.java
@@ -1,8 +1,35 @@
+/*
+ * Elabore um programa que solicite ao usuário três notas. Sendo assim: primeira prova, segunda prova
+ * e trabalho. Em seguida, calcule a média dessas notas. 
+ * Usando o operador ternário, determine se o aluno foi aprovado ou reprovado. 
+ * Faça a impressão da média e situação do aluno na tela.
+ */
+
 import java.util.Scanner;
 
 public class Atv2 {
     public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("\n|> Digite a primeira nota: ");
+        int nota1 = sc.nextInt();
 
+        System.out.println("|> Digite a segunda nota: ");
+        int nota2 = sc.nextInt();
+
+        System.out.println("|> Digite a nota do seu trabalho: ");
+        int nota_trab = sc.nextInt();
+
+        sc.close();
+
+        int calc_media;
+        calc_media = (nota1 + nota2 + nota_trab)/3;
+
+        System.out.println("\nA nota de media é igual a: " + calc_media + "\n");
+            if(calc_media >= 6){
+                System.out.println(" ==== O aluno foi: APROVADO ====");
+            }else{
+                System.out.println(" ==== O aluno foi REPROVADO ====");
+            }
     }
 }
